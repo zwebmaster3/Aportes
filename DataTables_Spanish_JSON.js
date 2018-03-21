f54cbdcb0e3173d35009dac1ea222d149d05eb37
zwebmaster3 <zoemrccavl93@gmail.com>
2018-03-20 18:11:50 -0600

    Delete index.html And add DataTables Spanish JSON

---
+++ DataTables_Spanish_JSON.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#example').DataTable( {
+        "language": {
+              "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+        }
+    } );
+} );
